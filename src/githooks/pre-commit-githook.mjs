0a998e1f1c73f47b5f201418f45349ee6d9c57d4
Carl Schader <carlschader@gmail.com>
2021-07-03 13:53:01 -0600

    pre-commit hook wasn't running testing

--- src/githooks/pre-commit-githook.mjs
+++ src/githooks/pre-commit-githook.mjs
@@ -1,8 +1,14 @@
 #! /usr/bin/env node
 
+import shell from 'shelljs';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
+
+if (shell.exec('npm run test').code !== 0) {
+    console.error("didn't pass testing");
+    shell.exit(1);
+}
 
 var packageJson = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), 'package.json'))
