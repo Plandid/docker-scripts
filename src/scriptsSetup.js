7c5eda824d420e4e99daaa3a0c60402e5f2a26af
carlschader <carlschader@gmail.com>
2021-07-03 17:08:26 -0600

    trying something new with the hooks

--- src/scriptsSetup.js
+++ src/scriptsSetup.js
@@ -37,9 +37,54 @@ exec(`git config core.hooksPath ${gitHooksPath}`, (error, stderr, stdout) => {
     console.log(stdout);
 });
 
+const postCommitData = `#! /usr/bin/env node
+
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+var packageJson = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), 'package.json'))
+);
+
+exec('git tag ' + packageJson.version, (error, stderr, stdout) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    if (stderr) {
+        console.error(stderr);
+        process.exit(1);
+    }
+    console.log(stdout);
+});`;
+
+const preCommitData = `#! /usr/bin/env node
+
+const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+exec('npm run test', (error, stderr, stdout) => {
+    console.log(stdout);
+    if (error | stderr) {
+        process.exit(1);
+    }
+});
+
+var packageJson = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), 'package.json'))
+);
+
+const [major, minor, patch] = packageJson.version.split('.');
+packageJson.version = major + '.' + minor + '.' + (parseInt(patch) + 1).toString();
+fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(packageJson, null, 2));`;
+
 try {
-    fs.writeFileSync(path.join(gitHooksPath, 'post-commit'), '#! /bin/sh\nnpx post-commit-githook');
-    fs.writeFileSync(path.join(gitHooksPath, 'pre-commit'), '#! /bin/sh\nnpx pre-commit-githook');
+    fs.writeFileSync(path.join(gitHooksPath, 'post-commit'), postCommitData);
+    fs.writeFileSync(path.join(gitHooksPath, 'pre-commit'), preCommitData);
 } catch (error) {
     console.error(error);
     exec(`rm -r ${gitHooksPath}`, (error, stderr, stdout) => {
