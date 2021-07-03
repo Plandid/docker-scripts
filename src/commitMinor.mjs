d0fb854d62010cbf231194408ebd6db66a6f49a3
Carl Schader <carlschader@gmail.com>
2021-07-03 14:07:52 -0600

    test

--- src/commitMinor.mjs
+++ src/commitMinor.mjs
@@ -19,6 +19,11 @@ if (!shell.which('git')) {
     shell.exit(1);
 }
 
+if (shell.exec('npm run test').code !== 0) {
+    console.error("didn't pass testing");
+    shell.exit(1);
+}
+
 const [major, minor, patch] = packageJson.version.split('.');
 packageJson.version = `${major}.${parseInt(minor) + 1}.${0}`;
 fs.writeFileSync(path.join(process.cwd(), 'package.json'), JSON.stringify(packageJson, null, 2));
