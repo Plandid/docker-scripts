7ae114f8bcdfe8b3a2ccaa064610da99d0ce2b9c
carlschader <carlschader@gmail.com>
2021-07-03 15:41:56 -0600

    removed the shell dependency from scriptsSetup.mjs

--- src/scriptsSetup.mjs
+++ src/scriptsSetup.mjs
@@ -1,36 +1,77 @@
 #! /usr/bin/env node
 
-import shell from 'shelljs';
+import { exec } from 'child_process';
 import path from 'path';
 import fs from 'fs';
-import process from 'process';
+import process, { stderr, stdout } from 'process';
 
 const gitHooksPath = path.join(process.cwd(), 'githooks')
 
-if (!shell.which('git')) {
-    console.error('this script requires git');
-    shell.exit(1);
-}
+exec(`mkdir ${gitHooksPath}`, (error, stderr, stdout) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    if (stderr) {
+        console.error(error);
+        process.exit(1);
+    }
+    console.log(stdout);
+});
 
-shell.mkdir(gitHooksPath);
-
-if (shell.exec(`git config core.hooksPath ${gitHooksPath}`).code !== 0) {
-    console.log('could not configure core.hooksPath');
-    shell.rm('-r', gitHooksPath);
-    shell.exit(1);
-}
+exec(`git config core.hooksPath ${gitHooksPath}`, (error, stderr, stdout) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    if (stderr) {
+        console.error(error);
+        process.exit(1);
+    }
+    console.log(stdout);
+});
 
 try {
     fs.writeFileSync(path.join(gitHooksPath, 'post-commit'), '#! /bin/sh\nnpx post-commit-githook');
     fs.writeFileSync(path.join(gitHooksPath, 'pre-commit'), '#! /bin/sh\nnpx pre-commit-githook');
 } catch (error) {
     console.error(error);
-    shell.rm('-r', gitHooksPath);
-    shell.exit(1);
+    exec(`rm -r ${gitHooksPath}`, (error, stderr, stdout) => {
+        if (error) {
+            console.error(error);
+            process.exit(1);
+        }
+        if (stderr) {
+            console.error(error);
+            process.exit(1);
+        }
+        console.log(stdout);
+    });
 }
 
-shell.chmod('+x', path.join(gitHooksPath, 'post-commit'));
-shell.chmod('+x', path.join(gitHooksPath, 'pre-commit'));
+exec(`chmod +x ${path.join(gitHooksPath, 'post-commit')}`, (error, stderr, stdout) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    if (stderr) {
+        console.error(error);
+        process.exit(1);
+    }
+    console.log(stdout);
+});
+
+exec(`chmod +x ${path.join(gitHooksPath, 'pre-commit')}`, (error, stderr, stdout) => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    if (stderr) {
+        console.error(error);
+        process.exit(1);
+    }
+    console.log(stdout);
+});
 
 var packageJson = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), 'package.json'))
