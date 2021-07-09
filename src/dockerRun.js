cc7ef6fcc8575d01c531112ce6ed8e0d982bc10d
carlschader <carlschader@gmail.com>
2021-07-09 04:28:14 -0600

    fix: no longer using dotenv

--- src/dockerRun.js
+++ src/dockerRun.js
@@ -5,8 +5,6 @@ const path = require('path');
 const fs = require('fs');
 const process = require('process');
 
-require('dotenv').config();
-
 const packageJson = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), 'package.json'))
 );
@@ -14,6 +12,7 @@ const packageJson = JSON.parse(
 const imageName = process.argv[2] ? process.argv[2] : packageJson.name;
 const version = process.argv[3] ? process.argv[3] : packageJson.version;
 
-const httpsParam = process.env.HTTPS_PORT ? ` -p 443:${process.env.HTTPS_PORT}` : "";
+const httpPort = process.argv[4] ? process.argv[4] : '8080';
+const httpsParam = process.argv[5] ? ` -p 443:${process.argv[5]}` : "";
 
-execSync(`docker run -d --rm --env-file .env -p 80:${process.env.PORT}${httpsParam} ${imageName}:${version}`);
+execSync(`docker run -d --rm --env-file .env -p 80:${httpPort}${httpsParam} ${imageName}:${version}`);
